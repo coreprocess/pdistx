7106ead1f866ee0f1ffd3b6644e1b84ad42b06fb
Nghia Lam <nghialam12795@gmail.com>
2021-03-04 17:19:43 +0700

    Setup module graphs

--- pyscriptpacker/pyscriptpacker/graph.py
+++ pyscriptpacker/pyscriptpacker/graph.py
@@ -1,4 +1,88 @@
+import os
 import sys
+import ast
+
+
+class ImportInfo(object):
+    """
+    A record of a name and the location of the import statement.
+    """
+
+    def __init__(self, name, filename, level):
+        self.name = name
+        self.filename = filename
+        self.level = level
+
+    def __repr__(self):
+        # Used for debugging (print to console)
+        return '%s(%r, %r, %r)' % (
+            self.__class__.__name__,
+            self.name,
+            self.filename,
+            self.level,
+        )
+
+
+class ImportFinder(ast.NodeVisitor):
+    """
+    This class is implemented as a NodeVisitor which will collect all the
+    import dependencies when visit a new file as root.
+
+    Reference: https://www.mattlayman.com/blog/2018/decipher-python-ast/
+    """
+
+    def __init__(self, file_name, file_path):
+        self.imports = []
+        self.file_name = file_name
+
+        with open(os.path.join(file_path, file_name)) as f:
+            root = ast.parse(f.read(), file_name)
+        self.visit(root)
+
+    def visit_Import(self, node):
+        for alias in node.names:
+            self._process(alias.name, None)
+
+    def visit_ImportFrom(self, node):
+        for alias in node.names:
+            fullname = '%s.%s' % (node.module,
+                                  alias.name) if node.module else alias.name
+            self._process(fullname, node.level)
+
+    def _process(self, full_name, level):
+        info = ImportInfo(full_name, self.file_name, level)
+        self.imports.append(info)
+
+
+class Module(object):
+    """
+    Node in a module dependency graph.
+    """
+
+    def __init__(self, module_name, file_name):
+        self.module_name = module_name
+        self.file_name = file_name
+        self.is_package = False
+        self.content = ''
+        self.imports = []
+
+    def to_dict(self):
+        data = {
+            'name': self.module_name,
+            'is_package': self.is_package,
+            'code': self.content,
+        }
+        return data
+
+    def __repr__(self):
+        # Used for debugging (print to console)
+        return '<%s: %s>:\nFile: %r\nPackage: %r\nCode: %r\n' % (
+            self.__class__.__name__,
+            self.module_name,
+            self.file_name,
+            self.is_package,
+            self.content,
+        )
 
 
 class ModuleGraph(object):
@@ -10,4 +94,47 @@ class ModuleGraph(object):
     BUILTIN_MODULES = sys.builtin_module_names
 
     def __init__(self):
-        pass
+        self.modules = {}
+
+    def parse_path(self, path):
+        if not os.path.exists(path):
+            return
+
+        for root, _, files in os.walk(path):
+            for file in files:
+                # TODO(Nghia Lam): exclude `setup.py` because some structure
+                # does put the `setup.py` outside of the module folder, not
+                # inside it. Do we have a better way to solve this?
+                if file.endswith('.py') and file != 'setup.py':
+                    self.parse_file(file, root)
+
+    def parse_file(self, file_name, file_path):
+        # NOTE(Nghia Lam): We did make sure the default file_path contain at
+        # least one python file, it means that the main folder in file_path is
+        # a module. But we need to make get all the parent scope of the module
+        # to avoid name clashes.
+        module_name = self._find_module_name(file_path)
+        module = Module(module_name, file_name)
+        module.imports = ImportFinder(file_name, file_path).imports
+        module.is_package = (file_name == '__init__.py')
+        with open(os.path.join(file_path, file_name), 'r') as file_data:
+            module.content = file_data.read()
+
+        self.modules[module_name] = module
+
+    def _find_module_name(self, file_path):
+        module_name = []
+
+        elements = file_path.split(os.path.sep)
+        while elements:
+            module_name.append(elements.pop())
+            # Stop when there's any python file
+            contain_python = any(
+                '.py' in file and file != 'setup.py'
+                for file in os.listdir(os.path.sep.join(elements)))
+            if not contain_python:
+                break
+        module_name.reverse()
+        module_name = '.'.join(module_name)
+
+        return module_name
--- pyscriptpacker/pyscriptpacker/packer.py
+++ pyscriptpacker/pyscriptpacker/packer.py
@@ -1,61 +1,21 @@
 import os
 import sys
-import ast
 
 from pyscriptpacker import utils
 from pyscriptpacker import graph
 
 
-class ImportInfo(object):
-    """A record of a name and the location of the import statement."""
-
-    def __init__(self, name, filename, level):
-        self.name = name
-        self.filename = filename
-        self.level = level
-
-    def __repr__(self):
-        return '%s(%r, %r, %r)' % (
-            self.__class__.__name__,
-            self.name,
-            self.filename,
-            self.level,
-        )
-
-
-class ImportFinder(ast.NodeVisitor):
-    """
-    This class is implemented as a NodeVisitor which will collect all the
-    import dependencies when visit a new file as root.
-
-    Reference: https://www.mattlayman.com/blog/2018/decipher-python-ast/
-    """
-
-    def __init__(self, file_name):
-        self.imports = []
-        self.file_name = file_name
-
-    def visit_Import(self, node):
-        for alias in node.names:
-            self._process(alias.name, None)
-
-    def visit_ImportFrom(self, node):
-        for alias in node.names:
-            fullname = '%s.%s' % (node.module,
-                                  alias.name) if node.module else alias.name
-            self._process(fullname, node.level)
-
-    def _process(self, full_name, level):
-        info = ImportInfo(full_name, self.file_name, level)
-        self.imports.append(info)
-
-
-def find_imports(file_name, file_path):
-    with open(os.path.join(file_path, file_name)) as f:
-        root = ast.parse(f.read(), file_name)
-    visitor = ImportFinder(file_name)
-    visitor.visit(root)
-    return visitor.imports
+def write_output(output_path, texts):
+    try:
+        with open(output_path, 'w') as output:
+            # TODO(Nghia Lam): Something wrong with new line characters '\n',
+            # it cannot write a new line ...
+            output.write(texts)
+    except IOError as e:
+        sys.stdout.write('Error: Cannot write to ' + output_path +
+                         '\nPlease make sure the directory is valid!!\n' +
+                         str(e))
+        sys.exit(1)
 
 
 def find_all_module_paths(library_paths):
@@ -80,19 +40,6 @@ def find_all_module_paths(library_paths):
     return all_module_paths
 
 
-def write_output(output_path, texts):
-    try:
-        with open(output_path, 'w') as output:
-            # TODO(Nghia Lam): Something wrong with new line characters '\n',
-            # it cannot write a new line ...
-            output.write(texts)
-    except IOError as e:
-        sys.stdout.write('Error: Cannot write to ' + output_path +
-                         '\nPlease make sure the directory is valid!!\n' +
-                         str(e))
-        sys.exit(1)
-
-
 def get_setup_module_code(python_version):
     return (utils.py2_setup_code
             if python_version == '2.7' else utils.py3_setup_code)
@@ -107,7 +54,6 @@ def pack_modules(product_name, library_paths):
         for root, _, files in os.walk(path):
             for file in files:
                 # Open file for reading
-                find_imports(file, root)
                 with open(os.path.join(root, file), 'r') as file_data:
                     file_text = file_data.read()
 
@@ -130,14 +76,6 @@ def pack_modules(product_name, library_paths):
 def pack(python_version, output_path, product_name, library_paths):
     main_script = 'import sys\n'
 
-    # Add modules to main_script
-    modules = pack_modules(product_name, library_paths)
-    main_script += ('_modules = [' +
-                    ','.join([str(module) for module in modules]) + ']')
-
-    # Setup module code
-    main_script += get_setup_module_code(python_version)
-
     module_graph = graph.ModuleGraph()
-
-    write_output(output_path, main_script)
+    for path in library_paths:
+        module_graph.parse_path(path)
