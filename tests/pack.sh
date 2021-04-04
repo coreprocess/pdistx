1d9d725f54d5241d54500d09191d59adb096b744
Nghia Lam <nghialam12795@gmail.com>
2021-04-04 13:39:35 +0700

    Update pack script in test cases

--- tests/pack.sh
+++ tests/pack.sh
@@ -4,9 +4,14 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+# install pyscriptpacker with require packages
+pushd ../
+python -m pip install -e .
+popd
+
 # project root
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 # pack libraries
 mkdir -p ./packed
-PYTHONPATH=.. python -m pyscriptpacker -m ./unpacked/tests/__main__.py tests,lib1,lib2,lib3,lib4 ./unpacked ./packed/libs.py
+PYTHONPATH=.. python -m pyscriptpacker -i -c -m ./unpacked/tests/__main__.py tests,lib1,lib2,lib3,lib4 ./unpacked ./packed/libs.py
