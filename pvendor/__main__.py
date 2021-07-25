d3018b703d3450ec483c4a1b5c94ff31d3bbb1ee
Niklas Salmoukas <niklas@salmoukas.com>
2021-07-25 13:06:52 -0700

    make removal of cwd conditional

--- pvendor/__main__.py
+++ pvendor/__main__.py
@@ -2,7 +2,8 @@ import sys
 
 # avoid having the cwd in the path
 # see https://docs.python.org/3/tutorial/modules.html#the-module-search-path
-sys.path.remove('')
+if '' in sys.path:
+    sys.path.remove('')
 
 import argparse
 from pathlib import Path
